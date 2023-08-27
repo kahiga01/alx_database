0fa6983c0606e39114681cc7dee2ec0e15956135
Kahiga01 <danielkahiga69@gmail.com>
2023-08-27 08:31:51 -0700

    List all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,9 @@
---a script that lists all rows of the table first_table from the database hbtn_0c_0
-SELECT * 
+/*
+a script that lists all rows of the table first_table
+from the database hbtn_0c_0
+*/
+
+-- SQL query to select all fields from the first_table
+SELECT *
 FROM hbtn_0c_0.first_table;
+
