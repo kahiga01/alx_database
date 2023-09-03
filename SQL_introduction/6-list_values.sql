9f6ce797dd4f388264dfc1304c6cfab936fc53bd
kahiga01 <you@example.com>
2023-09-03 08:48:10 -0700

    list all in table

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,9 +1,2 @@
-/*
-a script that lists all rows of the table first_table
-from the database hbtn_0c_0
-*/
-
--- SQL query to select all fields from the first_table
-SELECT *
-FROM hbtn_0c_0.first_table;
-
+--  lists all rows of the table first_table from the database hbtn_0c_0
+SELECT * FROM first_table;
